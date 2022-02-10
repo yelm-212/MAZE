ed747326e114acadc1712f862330963549f4d115
yelm <21yrshin@naver.com>
2022-02-11 05:05:23 +0900

    revised2

---
+++ fptrcalculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sum(int x, int y){
+    return x + y;
+}
+int sub(int x, int y){
+    return x - y;
+}
+int mult(int x, int y){
+    return x * y;
+}
+int div(int x, int y){
+    return x / y;
+}
+
+int main(void){
+
+
+    return 0;
+}
