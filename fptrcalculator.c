d4ce01ca5df942222acb27cfd994eaf999f48268
yelm <21yrshin@naver.com>
2022-02-11 05:42:38 +0900

    revised

--- fptrcalculator.c
+++ fptrcalculator.c
@@ -14,7 +14,42 @@ int div(int x, int y){
 }
 
 int main(void){
+    int a,b;
+    char op;
+    int res;
 
+    int (*fptr)(int, int);
+
+    printf("a :");
+    scanf("%d", &a);
+
+    printf("\nb :");
+    scanf("%d", &b);
+
+    printf("\n연산 종류 (+,-,*,/) :");
+    scanf("%c", &op);
+
+    switch (op)
+    {
+    case '+':
+        fptr = sum;
+        break;
+    case '-':
+        fptr = sub;
+        break;
+    case '*':
+        fptr = mult;
+        break;
+    case '/':
+        fptr = div;
+        break;
+    
+    default:
+        printf("Wrong operator...");
+        break;
+    }
+
+    printf("\na %c b = %d", op, res);
 
     return 0;
 }
